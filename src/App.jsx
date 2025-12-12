d6210795100f39ca8d0c891650336b5ca4c8069a
rudhar-cmd <rudhargupta51@gmail.com>
2025-12-12 14:25:12 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { Route,Routes, useNavigate} from 'react-router-dom'
 import Login from './pages/Login/Login'
-import Chat from './pages/chat/chat'
+import Chat from './pages/Chat/Chat'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 import { ToastContainer, toast } from 'react-toastify';
 import { onAuthStateChanged } from 'firebase/auth'
